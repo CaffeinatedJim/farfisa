af9fa3a343918bfac04f6f8d1cb18e7addab2d87
Jim Birch <jim@xenomedia.com>
2014-09-23 09:32:45 -0500

    Bootstrap Child theme

---
+++ wp-content/themes/bootstrap-child/footer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The theme footer
+ * 
+ * @package bootstrap-basic
+ */
+?>
+
+			</div><!--.site-content-->
+			
+			
+			<footer id="site-footer" role="contentinfo">
+				<div id="footer-row" class="row site-footer">
+					<div class="col-md-6 footer-left">
+						<?php 
+						if (!dynamic_sidebar('footer-left')) {
+							printf(__('Powered by %s', 'bootstrap-basic'), 'WordPress');
+							echo ' | ';
+							printf(__('Theme: %s', 'bootstrap-basic'), '<a href="http://okvee.net">Bootstrap Basic</a>');
+						} 
+						?> 
+					</div>
+					<div class="col-md-6 footer-right text-right">
+						<?php dynamic_sidebar('footer-right'); ?> 
+					</div>
+				</div>
+			</footer>
+		</div><!--.container page-container-->
+		
+		
+		<!--wordpress footer-->
+		<?php wp_footer(); ?> 
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-9711088-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+	</body>
+</html>
