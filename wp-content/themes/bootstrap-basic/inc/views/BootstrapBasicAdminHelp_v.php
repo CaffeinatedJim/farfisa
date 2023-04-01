d8df465ea015713fae66e5511caac3f393784e32
thejimbirch <git@jimbir.ch>
2023-04-01 07:59:40 -0700

    Upodates theme

--- wp-content/themes/bootstrap-basic/inc/views/BootstrapBasicAdminHelp_v.php
+++ wp-content/themes/bootstrap-basic/inc/views/BootstrapBasicAdminHelp_v.php
@@ -1,28 +1,48 @@
 <div class="wrap">
-    <h2>Bootstrap Basic help</h2>
+    <h2><?php _e('Bootstrap Basic help', 'bootstrap-basic'); ?></h2>
 
-    <h3>Menu</h3>
-    <p>To display menu correctly, please create at least 1 menu and set as primary and save.</p>
+    <h3><?php _e('Menu', 'bootstrap-basic'); ?></h3>
+    <p><?php _e('To display menu correctly, please create at least 1 menu and set as primary and save.', 'bootstrap-basic'); ?></p>
 
-    <h3>Bootstrap features</h3>
-    <p>This theme can use all Bootstrap classes, elements and styles.</p>
+    <h3><?php _e('Bootstrap features', 'bootstrap-basic'); ?></h3>
+    <p><?php 
+    printf(
+        /* translators: $1$s open link to Bootstrap doc, %2$s close link. */
+        __('This theme can use all %1$sBootstrap 3%2$s classes, elements and styles.', 'bootstrap-basic'),
+        '<a href="https://getbootstrap.com/docs/3.4/" target="bootstrap_doc">',
+        '</a>'
+    ); 
+    ?></p>
 
-    <h3>Responsive image</h3>
-    <p>For responsive image please add img-responsive class to img element. Example: <code>&lt;img src=&quot;...&quot; alt=&quot;&quot; class=&quot;img-responsive&quot;&gt;</code></p>
+    <h3><?php _e('Responsive image', 'bootstrap-basic'); ?></h3>
+    <p><?php 
+    printf(
+        /* translators: %1$s sample of responsive image HTML with class. */
+        __('For responsive image please add img-responsive class to img element. Example: %1$s', 'bootstrap-basic'),
+        '<code>&lt;img src=&quot;...&quot; alt=&quot;&quot; class=&quot;img-responsive&quot;&gt;</code>'
+    );
+    ?></p>
 
-    <h3>Responsive video</h3>
-    Cloak video element (video element or embeded video) with <code>&lt;div class=&quot;flexvideo&quot;&gt;...your video...&lt;/div&gt;</code>.
+    <h3><?php _e('Responsive video</h3>', 'bootstrap-basic'); ?>
+    <?php
+    printf(
+        /* translators: %1$s sample of responsive video HTML with class. */
+        __('Cloak video element (video element or embeded video) with %1$s.', 'bootstrap-basic'),
+        '<code>&lt;div class=&quot;flexvideo&quot;&gt;...&lt;/div&gt;</code>'
+    );
+    ?>
 
     <h3><?php _e('Bootstrap Basic Change log', 'bootstrap-basic'); ?></h3>
     <p>
-            <?php 
+        <?php 
+        printf(
             /* translators: %1$s: Open link, %2$s: Close link */
-            echo sprintf(__('You can see what was changed in each version or each commits on our %1$sGithub page%2$s.', 'bootstrap-basic'), '<a href="https://github.com/Rundiz-WP/bootstrap-basic" target="bb_commits">', '</a>'); 
-            ?> 
-            <?php _e('You can also see it on changelog.md file that come with the theme.', 'bootstrap-basic'); ?> 
+            __('You can see what was changed in each version or each commits on our %1$sGithub page%2$s.', 'bootstrap-basic'), 
+            '<a href="https://github.com/Rundiz-WP/bootstrap-basic" target="bb_commits">', '</a>'
+        ); 
+        ?> 
+        <?php _e('You can also see it on changelog.md file that come with the theme.', 'bootstrap-basic'); ?> 
     </p>
 
     <?php do_action('bootstrapbasic_theme_help_content'); ?>
-
-    <p style="margin-top: 20px;"><span style="font-size: 1.2rem;">&#128147;</span> If my theme can help you, your jobs, your projects please <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=9HQE4GVV4KTZE" target="donations_link">buy me some foods</a>.</p>
 </div>
